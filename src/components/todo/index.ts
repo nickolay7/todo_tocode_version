93dc6bb306733b9df14d53d0e57d84c4da8940f5
nickolay7 <shkelal50@gmail.com>
2022-11-29 09:15:02 +0300

    refactored

--- src/components/todo/index.ts
+++ src/components/todo/index.ts
@@ -1,2 +1,2 @@
 export * from './form';
-export * from './todoList';
+export * from './itemsList';
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,10 +3,18 @@ import { v4 as fakeId } from 'uuid';
 
 import { Container } from 'layout/Container';
 import { Todo } from 'types/types';
-import { Form, TodoList } from '../../components/todo';
+import { Form, ItemsList } from 'components/todo';
+import todosSeeder from 'seeders/todos.json';
 
 export const Home = () => {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const localStorageData = JSON.parse(localStorage.getItem('todos') || 'null');
+
+  const _items =
+    localStorageData && localStorageData.length > 0
+      ? localStorageData
+      : todosSeeder;
+
+  const [items, setItems] = useState<Todo[]>(_items || []);
 
   const addTodo = (title: string) => {
     const newTodo: Todo = {
@@ -15,23 +23,35 @@ export const Home = () => {
       isCompleted: false,
     };
 
-    setTodos([...todos, newTodo]);
+    setItems([...items, newTodo]);
   };
 
-  useEffect(() => console.log(todos));
-
   const toggleTodo = (id: string) => {
-    const newTodo = todos.map((todo) =>
+    const newTodo = items.map((todo) =>
       todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
     );
 
-    setTodos(newTodo);
+    setItems(newTodo);
   };
+
+  const removeTodo = (id: string) => {
+    const newTodos = items.filter((todo) => todo.id !== id);
+
+    setItems(newTodos);
+  };
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(items));
+  }, [items]);
 
   return (
     <Container className=''>
       <Form addTodo={addTodo} />
-      <TodoList items={todos} toggleTodo={toggleTodo} />
+      <ItemsList
+        items={items}
+        toggleTodo={toggleTodo}
+        removeTodo={removeTodo}
+      />
     </Container>
   );
 };
