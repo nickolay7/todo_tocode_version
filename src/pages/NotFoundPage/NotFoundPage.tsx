817b60f8706768c15472fef2a45394c7b11aa88e
nickolay7 <shkelal50@gmail.com>
2022-12-01 14:23:40 +0300

    delete NotFaundPage props

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,17 +1,16 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
-import { Container } from 'layout/Container'
+import { Container } from 'layout/Container';
 
-interface NotFoundPageProps {
-}
-
-export const NotFoundPage = ({}: NotFoundPageProps) => (
+export const NotFoundPage = () => (
   <Container>
-    <h1 className='ui-title-1 mb-4'>
-      404 - NotFound
-    </h1>
+    <h1 className='ui-title-1 mb-4'>404 - NotFound</h1>
     <p>
-      Page not found. Visit <Link className='ui-link' to='/'>Home page</Link>.
+      Page not found. Visit{' '}
+      <Link className='ui-link' to='/'>
+        Home page
+      </Link>
+      .
     </p>
   </Container>
-)
+);
